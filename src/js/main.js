3a6395b4dbee8e495f0e0e0187a05b42a22e749d
Adam Grider <adam.h.grider@gmail.com>
2019-04-14 07:24:38 -0400

    create gameboard

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
-console.log("Linked!")
+let allRows = document.querySelector('.rows')
+let numRows = 25;
+let numColumns = 40;
+
+//create function for user to set board size
+
+function createBoard() {
+  for (i = 0; i < numRows; i++) {
+    let newRow = document.createElement('div')
+    allRows.appendChild(newRow)
+    newRow.setAttribute('class',`row row-${i}`)
+    for (j = 0; j < numColumns; j++) {
+      let newSquare = document.createElement('div')
+      newRow.appendChild(newSquare)
+      newSquare.setAttribute('class',`square square-r${i}-c${j} square-${(i-1) * numColumns + j}`)
+    }
+  }
+}
+
+function firstSelection() {
+  
+}
+createBoard();
