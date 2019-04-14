7c3a067214e7bbcdb9399197b25791c0e1dc6df0
Adam Grider <adam.h.grider@gmail.com>
2019-04-14 17:45:31 -0400

    bugfixing

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+//Global Variables
 let allRows = document.querySelector('.rows')
 let numRows = 25;
 let numColumns = 40;
@@ -8,9 +9,11 @@ let firstSquare;
 let values = [];
 let finalValues = [];
 
+//Functions:
+//To do: Set board size
 
-//create function for user to set board size
 
+//Setup gameboard
 function createBoard() {
   time.innerHTML = parseInt(0,10)
   let marked = 0;
@@ -30,9 +33,26 @@ function createBoard() {
   }
 }
 
+//Clear a square with left click
+function clear(evt) {
+  evt.preventDefault();
+  if (squaresCleared < 1) {
+    firstSquare = evt.target
+    placeMines()
+    checkNeighbors(evt.target)
+    squaresCleared += 1;
+  } else {
+    squaresCleared += 1;
+    evt.target.removeAttribute('class')
+    evt.target.removeEventListener('click', clear)
+    checkNeighbors(evt.target)
+  }
+}
+
+//On first click, set mine locations
 function placeMines() {
+  console.log(firstSquare)
   let firstSquareId = parseInt(firstSquare.getAttribute('id').substr(2),10)
-  console.log(firstSquareId);
   firstSquare.removeAttribute('class')
   let mineCount = allMarkers;
   while (mineCount === allMarkers) {
@@ -40,72 +60,74 @@ function placeMines() {
     switch(seedVal){
       case 0:
         if ((firstSquareId >= numColumns) && (firstSquareId % numColumns !== 1)) {
-          firstMine = firstSquareId - numColumns - 1;
+          firstMine = (firstSquareId - numColumns - 1);
           console.log('first mine is top left')
           mineCount -= 1;
         }
         break;
       case 1:
         if (firstSquareId >= numColumns) {
-          firstMine = firstSquareId - numColumns;
+          firstMine = (firstSquareId - numColumns);
           console.log('first mine is top center')
           mineCount -= 1;
         }
         break;
       case 2:
         if ((firstSquareId >= numColumns) && (firstSquareId % numColumns !== 0)) {
-          firstMine = firstSquareId - numColumns + 1;
+          firstMine = (firstSquareId - numColumns + 1);
           console.log('first mine is top right')
           mineCount -= 1;
         }
         break;
       case 3:
         if (firstSquareId % numColumns !== 1) {
-          firstMine = firstSquareId - 1;
+          firstMine = (firstSquareId - 1);
           console.log('first mine is left center')
           mineCount -= 1;
         }
         break;
       case 4:
         if (firstSquareId % numColumns !== 0) {
-          firstMine = firstSquareId + 1;
+          firstMine = (firstSquareId + 1);
           console.log('first mine is right center')
           mineCount -= 1;
         }
         break;
       case 5:
         if ((firstSquareId % numColumns !== 1) && (firstSquareId <= ((numRows - 1) * numColumns))) {
-          firstMine = firstSquareId + numColumns - 1;
+          firstMine = (firstSquareId + numColumns - 1);
           console.log('first mine is bottom left')
           mineCount -= 1;
         }
         break;
       case 6:
         if (firstSquareId <= ((numRows - 1) * numColumns)) {
-          firstMine = firstSquareId + numColumns;
+          firstMine = (firstSquareId + numColumns);
           console.log('first mine is bottom center')
           mineCount -= 1;
         }
         break;
       case 7 || 8:
         if ((firstSquareId <= ((numRows - 1) * numColumns)) && (firstSquareId % numColumns !== 0)) {
-          firstMine = firstSquareId + numColumns + 1;
+          firstMine = (firstSquareId + numColumns + 1);
           console.log('first mine is bottom right')
           mineCount -= 1;
         }
         break;
       default:
-        console.log('something is broken - trying again')
+        console.log('a random number led to an invalid setup - trying again')
     }
   }
-  console.log(firstMine)
-  for (i = 0; i < (allMarkers - 1); i++) {
+  for (i = 1; i <= (allMarkers - 1); i++) {
     values.push("mine")
     mineCount -= 1
   }
-  for (i = 0; i < ((numColumns * numRows) - allMarkers - 1); i++) {
+  console.log(`The array has ${values.length} mines other than the first one`)
+  for (i = (allMarkers + 2); i <= (numColumns * numRows); i++) {
     values.push("tbd")
   }
+  console.log(`The array has ${values.length} values`)
+
   //shuffle the array
   let currentIndex = values.length, temporaryValue, randomIndex;
   while (0 !== currentIndex) {
@@ -115,26 +137,33 @@ function placeMines() {
     values[currentIndex] = values[randomIndex];
     values[randomIndex] = temporaryValue;
   }
+  //push shuffled values to array and recombine, mixing in the first click ("tbd") and  first mine ("mine")
+  console.log(`The first square clicked was ${firstSquareId}`)
+  console.log(`The first mine generated was ${firstMine}`)
+  console.log(values.slice(0,(Math.min(firstSquareId,firstMine) - 1)))
   finalValues.push(values.slice(0,(Math.min(firstSquareId,firstMine) - 1)))
   if (firstSquareId < firstMine) {
-    finalValues.push(['tbd'])
+    finalValues.push(["tbd"])
 } else {
-    finalValues.push(['mine'])
+    finalValues.push(["mine"])
 }
-  finalValues.push(values.slice((Math.min(firstSquareId, firstMine) - 1), (Math.max(firstSquareId,firstMine) - 1)))
+  console.log(values.slice((Math.min(firstSquareId,firstMine) - 1), (Math.max(firstSquareId, firstMine) - 2)))
+  finalValues.push(values.slice((Math.min(firstSquareId, firstMine) - 1), (Math.max(firstSquareId,firstMine) - 2)))
   if (firstSquareId > firstMine) {
-    finalValues.push(['tbd'])
+    finalValues.push(["tbd"])
   } else {
-    finalValues.push(['mine'])
+    finalValues.push(["mine"])
   }
-  finalValues.push(values.slice(Math.max(firstSquareId,firstMine) - 1))
+  console.log(values.slice((Math.max(firstSquareId, firstMine) - 2)))
+  finalValues.push(values.slice(Math.max(firstSquareId,firstMine) - 2))
   values = finalValues[0].concat(finalValues[1],finalValues[2],finalValues[3],finalValues[4])
+  console.log(values)
 
-
-
+//Start timer
   setInterval(function() { time.innerHTML = parseInt(time.innerHTML, 10) + 1 }, 1000)
 }
 
+//count the mines around any clicked square
 function checkNeighbors(input) {
   let clicked = parseInt(input.getAttribute('id').substr(2),10)
   console.log(values[clicked])
@@ -227,18 +256,7 @@ function checkNeighbors(input) {
   }
 }
 
-function clear(evt) {
-  evt.preventDefault();
-  if (squaresCleared < 1) {
-    firstSquare = evt.target
-    placeMines()
-    checkNeighbors(evt.target)
-    squaresCleared += 1;
-  } else {
-    squaresCleared += 1;
-    evt.target.removeAttribute('class')
-    evt.target.removeEventListener('click', clear)
-    checkNeighbors(evt.target)
-  }
-}
+//To do: place mine markers on right click
+
+//This will ultimately be automated as part of the setup function
 createBoard();
